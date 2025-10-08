0f97263c412289ab9484882cda450afb3d4fed50
zenin5479 <zenin5479@gmail.com>
2025-10-08 13:36:58 +0300

    Update 0.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@
 
 namespace ArraysAndFunctions
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
 }
