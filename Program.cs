ab84ba045046d91d91432a85afed53d8c314638e
zenin5479 <zenin5479@gmail.com>
2025-10-10 13:13:54 +0300

    Update 7.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -318,22 +318,25 @@ namespace ArraysAndFunctions
 
          Console.Write("Введите натуральное число с разными цифрами: ");
          int numbe = int.Parse(Console.ReadLine());
-
          char[] digits = numbe.ToString().ToCharArray();
-
          int maxIndex = 0, minIndex = 0;
-
          for (int i = 1; i < digits.Length; i++)
          {
-            if (digits[i] > digits[maxIndex]) maxIndex = i;
-            if (digits[i] < digits[minIndex]) minIndex = i;
+            if (digits[i] > digits[maxIndex])
+            {
+               maxIndex = i;
+            }
+
+            if (digits[i] < digits[minIndex])
+            {
+               minIndex = i;
+            }
          }
 
          if (maxIndex < minIndex)
             Console.WriteLine($"Максимальная цифра {digits[maxIndex]} левее минимальной {digits[minIndex]}");
          else if (minIndex < maxIndex)
             Console.WriteLine($"Минимальная цифра {digits[minIndex]} левее максимальной {digits[maxIndex]}");
-
 
          Console.ReadKey();
       }
