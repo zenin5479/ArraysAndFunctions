e2572e7b08441d5baf98717793c957a5ecb7640d
zenin5479 <zenin5479@gmail.com>
2025-10-17 10:08:32 +0300

    Update 23.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -284,7 +284,7 @@ namespace ArraysAndFunctions
          Console.WriteLine("Определение точки пересечения векторов");
          Console.WriteLine("--------------------------------------");
          Random randomline = new Random();
-         double xA = randomline.Next(-5, 5);
+         double xA = randomline.Next(-10, 10);
          double yA = randomline.Next(-5, 5);
          double xB = randomline.Next(-5, 5);
          double yB = randomline.Next(-5, 5);
