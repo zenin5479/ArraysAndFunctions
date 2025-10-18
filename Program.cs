2cf0d460c5ff4b81cdcc5926d9152480ed1cbc05
zenin5479 <zenin5479@gmail.com>
2025-10-18 15:03:44 +0300

    Update 25.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -273,13 +273,14 @@ namespace ArraysAndFunctions
 
          Console.WriteLine("|      Ввод         |     Построение пути         | ");
          Console.WriteLine("|Точка | Координата |   Точка    Расстояние       |"); //
-         
+
          register = 0;
-         //string res2 = arrayDistance[register].ToString("F2"); // 2 знака
-         //string res3 = arrayDistance[register].ToString("N3"); // 3 знака + разделители
+
          while (register < distance)
          {
             string res1 = string.Format("{0:f4}", arrayDistance[register]); // 4 знака
+            //string res2 = arrayDistance[register].ToString("F2"); // 2 знака
+            //string res3 = arrayDistance[register].ToString("N3"); // 3 знака + разделители
             Console.WriteLine("|   " + register + "  |  [" + arrayXInput[register] + "; " + arrayYInput[register] +
                               "]   " + "|  [" + arrayX[register] + "; " + arrayY[register] + "]" + " = " + res1 + "|");
 
